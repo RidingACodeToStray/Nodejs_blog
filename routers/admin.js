4f5935c383664888d29b0d065ae47c4d3bcec6c3
ming <ming@appsnt.com>
2018-04-01 15:44:59 +0800

    NodeJS博客实战12_后台管理功能和界面搭建

--- routers/admin.js
+++ routers/admin.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/user',function(req,res,next){
-	res.send('admin-User');
+router.use(function(req,res,next){
+	if(!req.userInfo.isAdmin){
+		res.send('对不起，只有管理员才可以进入后台管理');
+	}
+	next();
+})
+
+router.get('/',function(req,res,next){
+	res.render('admin/index',{
+		userInfo:req.userInfo
+	});
 })
 
 module.exports = router;
